2747d0d305c05d3f193479b734f799a267e9f552
git-boiz <50253167+git-boiz@users.noreply.github.com>
2023-06-06 17:55:19 -0400

    wpf time

--- RLaunchWPF/Window1.xaml.cs
+++ RLaunchWPF/Window1.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RLaunchWPF {
+namespace RLaunchWPF
+{
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
             InitializeComponent();
-        }
-
-        void addInstance_Click(object sender, RoutedEventArgs e) {
-
         }
     }
 }
