564ff754a4c356378d6391ef21be416753bfa8aa
git-boiz <50253167+git-boiz@users.noreply.github.com>
2023-06-08 15:48:49 -0400

    json data is loaded half correctly

---
+++ RLaunchWPF/Game.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace RLaunch {
+    public class Game {
+
+        public string name { get; set; }
+        public string ver { get; set; }
+        public string desc { get; set; }
+        public string exe { get; set; }
+
+        public string? img;
+
+        public Game(string name, string ver, string desc, string exe, string? img = null) {
+            this.name = name;
+            this.ver = ver;
+            this.desc = desc;
+            this.exe = exe;
+            this.img = img;
+        }
+
+
+        public void Save() {
+            string json = JsonConvert.SerializeObject(this);
+            File.WriteAllText("data/meta/" + $"{this.name}{this.ver}.json", json);
+        }
+
+        public static Game Load(string filename) {
+            string json = File.ReadAllText($"{filename}");
+            return JsonConvert.DeserializeObject(json, typeof(Game)) as Game;
+        }
+
+    }
+}
--- RLaunchWPF/MainWindow.xaml.cs
+++ RLaunchWPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using RLaunch;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,12 +20,27 @@ namespace RLaunchWPF {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        // public Game[] games = Array.Empty<Game>();
+
         public MainWindow() {
             InitializeComponent();
+            
+            if (!Directory.Exists("data/meta")) {
+                Directory.CreateDirectory("data/meta");
+            }
+
+            foreach (string file in Directory.GetFiles("data/meta")) {
+                Game game = Game.Load(file);
+                GameList.Items.Add(game); 
+            }
+
+
         }
 
         void addInstance_Click(object sender, RoutedEventArgs e) {
-
+            Window1 addInst = new Window1();
+            addInst.ShowDialog();
         }
     }
 }
